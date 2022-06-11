deaf780df8e25a21b3453b6fd1f1073cad3930d3
Jibesh Kumar Panda <99865910+Jibesh04@users.noreply.github.com>
2022-06-12 00:34:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function menuOnClick() {
+    document.getElementById("container").classList.toggle("active");
+    document.getElementById("navMain").classList.toggle("active");
+    document.getElementById("menuBackground").classList.toggle("activeBG");
+}
